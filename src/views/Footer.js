c810b24d729ebc8cb5cc09cc862019292d767e28
Amanda Woods <acwoods314@gmail.com>
2022-05-11 19:51:48 -0700

    Add files via upload

---
+++ src/views/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className='footer'>
+            <p>Copyright 2022 of Christian Luciano, Amanda Woods, and Craig Caparraz</p>
+            <p>Image courtesy of Element5 Digital on Unsplash</p>
+        </div>
+    )
+}
+
+export default Footer
---
+++ src/views/Main.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Main () {
+    return (
+        <div className='main'>
+            <h1>Grade School</h1>
+            <img src='../images/element5-digital-OyCI7Y4y0Bk-unsplash.jpg' alt='School desk with apple and supplies' />
+            <h3>California's Premier Ranking App for Schools and Districts</h3>
+        </div>
+    )
+}
+
+export default Main
