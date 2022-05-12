e5c821b35be96f978e99a75fa945e1ec7ff9dc3d
Craig Caparaz <cappuccino1890@Craigs-MacBook-Air.local>
2022-05-12 00:37:44 -0700

    second commit

--- FrontEnd/src/views/schoolsDistricts/School.js
+++ FrontEnd/src/views/schoolsDistricts/School.js
@@ -1,9 +1,34 @@
 import React from 'react';
+import Navbar from '../NavBar';
+import Footer from '../Footer';
 
 const School = () => {
     return (
-        <div>
-                
+                <div classNAme="schoolPage">
+            <Navbar />
+            <div className="school">
+            <div className="card">
+                <h1>School name</h1>
+                <img src="https://placekitten.com/200/300" />
+                <h2>District name</h2>
+                <p>text</p>
+            </div>
+
+            <div className="card">
+                <h1>School name</h1>
+                <img src="https://placekitten.com/200/300" />
+                <h2>District name</h2>
+                <p>text</p>
+            </div>
+
+            <div className="card">
+                <h1>School name</h1>
+                <img src="https://placekitten.com/200/300" />
+                <h2>District name</h2>
+                <p>text</p>
+            </div>
+            </div> 
+        <Footer /> 
         </div>
     )
 }
