3ee085e7a5143e843c8c8284932ef25e80c8aea1
LuckyShmucky <christianluciano567@gmail.com>
2022-05-12 19:50:03 -0700

    features/ full crud capabilities for districts goals/ shcema is made but needs to be updated

--- BackEnd/controllers/DistrictController.js
+++ BackEnd/controllers/DistrictController.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const District = require('../models/Districts')
 
-
+//Get all districts
 router.get('/', async (req, res) =>{
     try{
       const allDistricts = await District.find()
@@ -15,10 +15,24 @@ router.get('/', async (req, res) =>{
     }
 })
 
-//get route to test if connection is working 
+//Get one district by Id
+router.get('/:id', async (req, res) =>{
+    try{
+      const foundDistrict = await District.findById(req.params.id)
+        res.status(200).json({
+            data: foundDistrict
+        })
+    } catch(err){
+        res.status(200).json(err)
+    }
+})
+
+
+
 
 //Create a district
 router.post('/', async (req, res) =>{
+    //if the body of the request is a falsy value the function will end
     if (req.body.name === false) return;
     try{
       const newDistrict = await District.create(req.body)
@@ -32,7 +46,7 @@ router.post('/', async (req, res) =>{
 })
 
 
-
+//find and delete a district by Id
 router.delete('/:id', async (req, res) =>{
     try{
        const deletedDistrict = await District.findByIdAndDelete(req.params.id)
@@ -40,6 +54,21 @@ router.delete('/:id', async (req, res) =>{
             message: `Deleted district with id:${req.params.id}`,
             data: deletedDistrict
         })
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//find and delete a district by Id
+router.put('/:id', async (req, res) => {
+    //if the body of the request is a falsy value the function will end
+    if (req.body === false) return;
+    try{
+       const updatedDistrict = await District.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        res.status(200).json({
+            message: `Updated district with id:${req.params.id}`,
+            data: updatedDistrict
+        } )
     } catch(err){
         res.status(500).json(err)
     }
