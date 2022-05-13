1726e520c544f04a465582fdd65f2c7f636e9ef7
LuckyShmucky <christianluciano567@gmail.com>
2022-05-12 18:23:02 -0700

    features/ districts schema created and controller for read/create is working

--- BackEnd/controllers/DistrictController.js
+++ BackEnd/controllers/DistrictController.js
@@ -1,10 +1,34 @@
 const express = require('express')
 const router = express.Router()
+const District = require('../models/Districts')
 
-router.get('/', (req, res) =>{
-    res.status(200).json({
-        message: "These are the districts"
-    })
+
+router.get('/', async (req, res) =>{
+    try{
+      const allDistricts = District.find()
+        res.status(200).json({
+            message: "These are the districts",
+            data: allDistricts
+        })
+    } catch(err){
+        res.status(200).json(err)
+    }
+})
+
+
+//get route to test if connection is working 
+
+//Create a district
+router.post('/', async (req, res) =>{
+    try{
+      const newDistrict = District.create(req.body)
+      res.status(200).json({
+          message: "new District created",
+          data: newDistrict
+      })
+    } catch(err){
+        res.status(500).json(err)
+    }
 })
 
 module.exports = router
