6973153e50f6b831a94a3b311635a6f3bc418872
LuckyShmucky <christianluciano567@gmail.com>
2022-05-12 19:21:35 -0700

    features/ delete route working

--- BackEnd/controllers/DistrictController.js
+++ BackEnd/controllers/DistrictController.js
@@ -5,7 +5,7 @@ const District = require('../models/Districts')
 
 router.get('/', async (req, res) =>{
     try{
-      const allDistricts = District.find()
+      const allDistricts = await District.find()
         res.status(200).json({
             message: "These are the districts",
             data: allDistricts
@@ -19,9 +19,9 @@ router.get('/', async (req, res) =>{
 
 //Create a district
 router.post('/', async (req, res) =>{
-    // if (req.body.name === null) return;
+    if (req.body.name === false) return;
     try{
-      const newDistrict = District.create(req.body)
+      const newDistrict = await District.create(req.body)
       res.status(200).json({
           message: "new District created",
           data: newDistrict
@@ -31,6 +31,18 @@ router.post('/', async (req, res) =>{
     }
 })
 
-router.delete
+
+
+router.delete('/:id', async (req, res) =>{
+    try{
+       const deletedDistrict = await District.findByIdAndDelete(req.params.id)
+        res.status(200).json({
+            message: `Deleted district with id:${req.params.id}`,
+            data: deletedDistrict
+        })
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router
