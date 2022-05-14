5fd4ffdb13970e9734234f797a7c162015d26614
Craig Caparaz <cappuccino1890@Craigs-MacBook-Air.local>
2022-05-14 10:05:57 -0700

    resolving pickle

---
+++ FrontEnd/src/views/schoolsDistricts/District.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Navbar from '../NavBar';
+import Footer from '../Footer';
+
+const District = () => {
+    return (
+        <div classNAme="district">
+            <Navbar />
+            <div>
+            <div className="card">
+                <h1>District name</h1>
+                <img src="https://placekitten.com/200/300" />
+                <ul>
+                    <li><a>School Name</a></li>
+                    <li><a>School Name</a></li>
+                    <li><a>School Name</a></li>
+                    <li><a>School Name</a></li>
+                  
+                </ul>
+            </div>
+
+            <div className="card">
+                <h1>District name</h1>
+                <img src="https://placekitten.com/200/300" />
+                <ul>
+                    <li><a>School Name</a></li>
+                    <li><a>School Name</a></li>
+                    <li><a>School Name</a></li>
+                    <li><a>School Name</a></li>
+                  
+                </ul>
+            </div>
+
+            <div className="card">
+                <h1>District name</h1>
+                <img src="https://placekitten.com/200/300" />
+                <ul>
+                    <li><a>School Name</a></li>
+                    <li><a>School Name</a></li>
+                    <li><a>School Name</a></li>
+                    <li><a>School Name</a></li>
+                  
+                </ul>
+            </div>
+            </div>
+            
+        <Navbar />
+        </div>
+    )
+}
+
+export default District
