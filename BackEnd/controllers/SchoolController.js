6dff6c7ddf65dcec5f36cdc67980b5b7b184f311
LuckyShmucky <christianluciano567@gmail.com>
2022-05-14 11:35:01 -0700

    features/ all models made goals/ comments are not being posted correctly to database (needs fix)

--- BackEnd/controllers/SchoolController.js
+++ BackEnd/controllers/SchoolController.js
@@ -3,6 +3,8 @@ const express = require('express')
 const router = express.Router()
 const School = require('../models/School')
 
+
+// Get all schools
 router.get('/', async (req, res) =>{
     try{
       const allSchools = await School.find()
@@ -15,6 +17,19 @@ router.get('/', async (req, res) =>{
     }
 })
 
+// This route is able to find one School by its name
+router.get('/:name', async (req, res) =>{
+    try{
+      const foundSchool = await School.findOne({'name': req.params.name})
+        res.status(200).json({
+            data: foundSchool
+        })
+    } catch(err){
+        res.status(200).json(err)
+    }
+})
+
+
 
 //Create a school
 router.post('/', async (req, res) =>{
@@ -23,7 +38,7 @@ router.post('/', async (req, res) =>{
     try{
       const newSchool = await School.create(req.body)
       res.status(200).json({
-          message: "new School created",
+          message: `new School named ${req.body.name} created`,
           data: newSchool
       })
     } catch(err){
@@ -31,5 +46,51 @@ router.post('/', async (req, res) =>{
     }
 })
 
+// Creating comments on Schools
+router.post('/:id', async (req, res) =>{
+    console.log(req.body)
+    try {
+        const comment = await School.Comment.create(req.body)
+        let foundSchool = await Place.findById(req.params.id)
+        foundSchool.comments.push(comment.id)
+        await foundSchool.save()
+        res.status(200).json(`Created comment by ${req.body.author}`)
+        process.exit()
+        //    res.redirect(`/places/${req.params.id}`)
+    } catch(err){
+        res.status(500).json(err)
+    }
+  
+  })
+
+
+
+
+//find and delete a district by Id
+router.delete('/:id', async (req, res) =>{
+    try{
+       const deletedSchool = await School.findByIdAndDelete(req.params.id)
+        res.status(200).json({
+            message: `Deleted School with id:${req.params.id}`,
+            data: deletedSchool
+        })
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
+
+router.put('/:id', async (req, res) => {
+    //if the body of the request is a falsy value the function will end
+    if (req.body === false) return;
+    try{
+       const updatedSchool = await School.findByIdAndUpdate(req.params.id, req.body, {new: true})
+        res.status(200).json({
+            message: `Updated School with id:${req.params.id}`,
+            data: updatedSchool
+        } )
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
 
 module.exports = router
--- BackEnd/models/School.js
+++ BackEnd/models/School.js
@@ -14,10 +14,16 @@ const schoolSchema = new Schema ({
     },
     image: {type: String, required: true},
     city: {type: String, required: true},
-       
+    comments: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Comment"
+        }]   
 }, {toJSON: {virtuals: true}})
 
 const School = mongoose.model('School', schoolSchema)
 module.exports = School
+module.exports.School = require('./School')
+module.exports.Comment = require('./Comment')
+// module.exports = require('./Comment')
 
 
