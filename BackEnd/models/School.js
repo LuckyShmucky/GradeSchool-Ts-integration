fa00956967a693e9fad7e474e065a2545f6978e2
LuckyShmucky <christianluciano567@gmail.com>
2022-05-16 15:14:02 -0700

    feature/ comments populate the route that shows individual schools by Id

--- BackEnd/models/School.js
+++ BackEnd/models/School.js
@@ -5,7 +5,7 @@ const { Schema } = mongoose
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopolgy: true
-})
+})  
 
 const schoolSchema = new Schema ({
     name: {
