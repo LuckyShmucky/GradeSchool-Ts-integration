2dd5ed4ef116a9887018c7a2f0ae881389e1172d
LuckyShmucky <christianluciano567@gmail.com>
2022-05-16 16:02:15 -0700

    features/ cors npm package added and enabled goals/ on front end promise status stays pending when fetching data from api

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,15 +1,15 @@
 //Dependencies
 require('dotenv').config()
 const express = require('express')
-const res = require('express/lib/response')
 const mongoose = require('mongoose')
 const app = express()
-
+const cors = require('cors')
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
     function(){ console.log('connected to mongoDB ') }
   )
 
 // CONFIGURATION / MIDDLEWARE 
+app.use(cors)
 app.use(express.json())
 app.use(express.static('public'))
 app.use(express.urlencoded({extended: true}))
