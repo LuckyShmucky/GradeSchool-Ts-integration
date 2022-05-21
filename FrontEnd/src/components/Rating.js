61deb1ff21b34abb973cf732a867766038a031fa
LuckyShmucky <christianluciano567@gmail.com>
2022-05-21 09:46:32 -0700

    Features/ refractor how stars component has access to stars array

--- FrontEnd/src/components/Rating.js
+++ FrontEnd/src/components/Rating.js
@@ -3,9 +3,12 @@ import React, { useEffect, useState } from "react";
 
 const Stars = (props) => {
   // const [stars, setStars] = useState([]) 
-  let starNumbersArr = []
-  useEffect(() =>{
+  const [starArr, setStarArr] = useState([]) 
+  const initialStarArr = []
 
+  // this runs on mount
+  useEffect(() =>{
+    //fetching an individual school that is populated by its comments
     const getSchools = async () => {
       const response = await fetch(`https://back-end-for-grade-school.herokuapp.com/schools/${props.id}`, {
         method: 'GET',
@@ -16,26 +19,41 @@ const Stars = (props) => {
       })
       const schoolPopulated = await response.json()
       
-      // console.log(schoolPopulated.data.comments)
-      schoolPopulated.data.comments.map(comment => starNumbersArr.push(comment.stars))
-      console.log(starNumbersArr)
+     //mapping out all the comments and putting the star number in 
+      schoolPopulated.data.comments.map(comment => {
+        // console.log(comment.stars)
+        initialStarArr.push(comment.stars)
+      
+      setStarArr(initialStarArr)
+      // console.log(starArr)
+      })
+      // console.log(starNumbersArr, 'stars array for individual card')
     }
     getSchools()
   }, [])
-  let rating = <div className="inactive">no rating</div>;
-  const star = () => {
-    let stars = "";
-    for (let i = 0; i < props.school; i++) {
-      stars += "⭐️";
-    }
-    // console.log(stars);
-    return <h2>Rating: {stars}</h2>;
-  };
+    
+  useEffect(() => {
+    console.log(starArr)
+    let rating = <div className="inactive">no rating</div>;
+    // starArr.length > 0 ?
+    // console.log('you dont have any stars') :
+    // console.log(`you have ${starArr.length} stars`);
+  }, [starArr])
+
   return (
-  <div>
-    {star()}
+    <div>
+    {/* {star()} */}
  </div>
   )
 };
 
 export default Stars;
+
+// const star = () => {
+//   let stars = "";
+//   for (let i = 0; i < props.school; i++) {
+//     stars += "⭐️";
+//   }
+//   // console.log(stars);
+//   return <h2>Rating: {stars}</h2>;
+// };
