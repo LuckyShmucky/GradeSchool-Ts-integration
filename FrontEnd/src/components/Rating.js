75531777d34339978c60735a867ea4f81d50b71e
LuckyShmucky <christianluciano567@gmail.com>
2022-05-21 09:07:53 -0700

    features/ stars components successfully has access to the stars field from their respective comments

--- FrontEnd/src/components/Rating.js
+++ FrontEnd/src/components/Rating.js
@@ -1,22 +1,27 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 
 const Stars = (props) => {
-  //AVERAGE is the default value of every school added to the school page
-  // console.log(props.id)
-  const getSchools = async () => {
-  const response = await fetch(`https://back-end-for-grade-school.herokuapp.com/schools/${props.id}`, {
-    method: 'GET',
-    mode: 'cors',
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  })
-    const schoolPopulated = response.json()
+  // const [stars, setStars] = useState([]) 
+  let starNumbersArr = []
+  useEffect(() =>{
 
-    // console.log(schoolPopulated)
-  }
-getSchools()
+    const getSchools = async () => {
+      const response = await fetch(`https://back-end-for-grade-school.herokuapp.com/schools/${props.id}`, {
+        method: 'GET',
+        mode: 'cors',
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      const schoolPopulated = await response.json()
+      
+      // console.log(schoolPopulated.data.comments)
+      schoolPopulated.data.comments.map(comment => starNumbersArr.push(comment.stars))
+      console.log(starNumbersArr)
+    }
+    getSchools()
+  }, [])
   let rating = <div className="inactive">no rating</div>;
   const star = () => {
     let stars = "";
