3be67e365b9e9cf0cac4c7963610aa7802a99ad0
LuckyShmucky <christianluciano567@gmail.com>
2022-05-21 10:43:08 -0700

    Features/ stars are being generated based on the average number of stars provided from Database

--- FrontEnd/src/components/Rating.js
+++ FrontEnd/src/components/Rating.js
@@ -5,10 +5,10 @@ const Stars = (props) => {
   // const [stars, setStars] = useState([]) 
   const [starArr, setStarArr] = useState([]) 
   const initialStarArr = []
-
+const [rating, setRating] = useState('Not yet Rated')
   // this runs on mount
   useEffect(() =>{
-    //fetching an individual school that is populated by its comments
+    //fetching an individual school by an id that is passed by props and is populated by its comments
     const getSchools = async () => {
       const response = await fetch(`https://back-end-for-grade-school.herokuapp.com/schools/${props.id}`, {
         method: 'GET',
@@ -19,30 +19,53 @@ const Stars = (props) => {
       })
       const schoolPopulated = await response.json()
       
-     //mapping out all the comments and putting the star number in 
+     //mapping out all the comments and pushing the stars number to initialStarArr
       schoolPopulated.data.comments.map(comment => {
-        // console.log(comment.stars)
         initialStarArr.push(comment.stars)
-      
-        // console.log(starArr)
       })
+      //setting state to the array with all the pushed numbers
       setStarArr(initialStarArr)
-      // console.log(starNumbersArr, 'stars array for individual card')
+      
     }
     getSchools()
+    
   }, [])
     
   useEffect(() => {
     console.log(starArr)
-    let rating = <div className="inactive">no rating</div>;
-    // starArr.length > 0 ?
-    // console.log('you dont have any stars') :
-    // console.log(`you have ${starArr.length} stars`);
+    
+    let stars = ''
+
+    if(starArr.length >= 1){
+      let sum = 0;
+        // let sumOfRatings = starArr.map(star => {
+        //   let sum = 0
+        //   sum += star
+        //   console.log(sum)
+        // })
+        for (let i = 0; i < starArr.length; i++){
+          sum += starArr[i]
+        }
+        
+        let averageRating = sum / starArr.length
+        // console.log(averageRating)
+
+        //  averageRating.map(star => {
+        //    stars += "⭐️";
+        //  })
+        for (let i = 0; i < averageRating; i++){
+          stars += '⭐'
+        }
+        console.log(stars)
+        setRating(stars)
+       }
+       console.log(stars)
   }, [starArr])
 
   return (
     <div>
     {/* {star()} */}
+    {rating}
  </div>
   )
 };
